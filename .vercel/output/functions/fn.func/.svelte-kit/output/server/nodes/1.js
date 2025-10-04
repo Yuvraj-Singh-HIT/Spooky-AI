5fd6e0ca5c0b0c372c5e5b3c5d075d3d37ce04fe
Yuvraj Singh <yuvrajseptember@gmail.com>
2025-10-04 20:19:32 +0530

    Switch to adapter-auto for Vercel deployment

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.a3c36133.js","_app/immutable/chunks/index.07784548.js","_app/immutable/chunks/singletons.52539acb.js"];
-export const stylesheets = [];
-export const fonts = [];
